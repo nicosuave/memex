c416d50417d22bc4f2b5b5b04cb40fabf0a3cf9f
Yannick Augenstein <yannick.augenstein@me.com>
2026-09-19 22:55:25 +0200

    Keep migration module addition independent of backfill module

--- src/lib.rs
+++ src/lib.rs
@@ -28,10 +28,11 @@ pub mod tui;
 pub mod types;
 pub mod usage;
 pub mod vector;
-pub mod vector_transfer;
 pub mod watch;
 pub mod web;
 pub mod web_auth;
+
+pub mod vector_transfer;
 
 #[cfg(test)]
 pub mod test_support;
